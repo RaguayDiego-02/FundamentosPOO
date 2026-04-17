336f997cf7115fa061aec6129a41f5ed8adb9fee
Diego Raguay <draguay-2026067kinal.edu.gt>
2026-04-17 11:26:38 -0600

    000 Primer commit del proyecto

---
+++ src/org/diegoraguay/system/Principal.java
@@ -0,0 +1,13 @@
+package org.diegoraguay.system;
+
+/**
+ *
+ * @author Diego Raguay
+ */
+public class Principal {
+    //main y tabulador para escribir esto
+    public static void main(String[] args) {
+        //sout mas tabulador para escribir esto 
+        System.out.println("Hola mundo estoy en kinal");
+    }
+}
